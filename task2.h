ce1fb2834dd5f6cb26a151ea8436cec75a4ed1be
Bella Karn <143955427+bella-karn@users.noreply.github.com>
2025-04-14 11:12:24 -0400

    Update task2.h

--- task2.h
+++ task2.h
@@ -9,6 +9,7 @@
 
 #define MAX_PKT 4 /* determines packet size in bytes */
 #define MAX_EVENTS 64
+#define MAX_SEQ 1
 
 typedef enum {false, true} boolean; /* boolean type */
 typedef unsigned int seq_nr; /* sequence of ack numbers */
@@ -37,7 +38,7 @@ typedef struct
 } context_t;
 
 // Protocol 3 functions
-void sender3(context_t *ctx, long long *tx_cnt, long long *rx_cnt);
+void sender3(context_t *ctx, long long *tx_cnt, long long *rx_cnt, long long *total_rtt);
 void receiver3(context_t *ctx);
 
 /* Pass the frame to the physical layer for transmission. */
@@ -46,15 +47,9 @@ void to_physical_layer(context_t *ctx, frame *s);
 /* Go get an inbound frame from the physical layer and copy it to r. */
 void from_physical_layer(context_t *ctx, frame *r);
 
-void wait_for_event(context_t *ctx, event_type *event);
-
-/* Start the clock running and enable the timeout event. */
-void start_timer(seq_nr k);
-
-/* Stop the clock and disable the timeout event. */
-void stop_timer(seq_nr k);
+void wait_for_event(context_t *ctx, event_type *event, int timeout_ms);
 
 /* Macro inc is expanded in-line: Increment k circularly. */
 #define inc(k) if(k < MAX_SEQ) k = k + 1; else k = 0
 
-#endif TASK2_H
+#endif // TASK2_H
